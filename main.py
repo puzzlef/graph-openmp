6d88b70019c1a914fa2c2cc82cf1c38a30850d29
Subhajit Sahu <wolfram77@gmail.com>
2022-12-10 19:43:34 +0530

    :bug: update expt setup

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# https://www.kaggle.com/wolfram77/puzzlef-louvain-openmp-static-vs-dynamic
+# https://www.kaggle.com/wolfram77/puzzlef-graph-openmp
 import os
 from IPython.display import FileLink
-src="louvain-openmp-static-vs-dynamic"
+src="graph-openmp"
 inp="/kaggle/input/graphs"
 out="{}.txt".format(src)
 !printf "" > "$out"
@@ -15,20 +15,16 @@ display(FileLink(out))
 
 # Run
 !g++ -std=c++17 -O3 -fopenmp main.cxx
-!stdbuf --output=L ./a.out $inp/web-Stanford.mtx      2>&1 | tee -a "$out"
-!stdbuf --output=L ./a.out $inp/web-BerkStan.mtx      2>&1 | tee -a "$out"
-!stdbuf --output=L ./a.out $inp/web-Google.mtx        2>&1 | tee -a "$out"
-!stdbuf --output=L ./a.out $inp/web-NotreDame.mtx     2>&1 | tee -a "$out"
-!stdbuf --output=L ./a.out $inp/soc-Slashdot0811.mtx  2>&1 | tee -a "$out"
-!stdbuf --output=L ./a.out $inp/soc-Slashdot0902.mtx  2>&1 | tee -a "$out"
-!stdbuf --output=L ./a.out $inp/soc-Epinions1.mtx     2>&1 | tee -a "$out"
-!stdbuf --output=L ./a.out $inp/coAuthorsDBLP.mtx     2>&1 | tee -a "$out"
-!stdbuf --output=L ./a.out $inp/coAuthorsCiteseer.mtx 2>&1 | tee -a "$out"
-!stdbuf --output=L ./a.out $inp/soc-LiveJournal1.mtx  2>&1 | tee -a "$out"
-!stdbuf --output=L ./a.out $inp/coPapersCiteseer.mtx  2>&1 | tee -a "$out"
-!stdbuf --output=L ./a.out $inp/coPapersDBLP.mtx      2>&1 | tee -a "$out"
-!stdbuf --output=L ./a.out $inp/indochina-2004.mtx    2>&1 | tee -a "$out"
-!stdbuf --output=L ./a.out $inp/italy_osm.mtx         2>&1 | tee -a "$out"
-!stdbuf --output=L ./a.out $inp/great-britain_osm.mtx 2>&1 | tee -a "$out"
-!stdbuf --output=L ./a.out $inp/germany_osm.mtx       2>&1 | tee -a "$out"
-!stdbuf --output=L ./a.out $inp/asia_osm.mtx          2>&1 | tee -a "$out"
+!stdbuf --output=L ./a.out $inp/GAP-road.mtx       2>&1 | tee -a "$out"
+!stdbuf --output=L ./a.out $inp/GAP-twitter.mtx    2>&1 | tee -a "$out"
+!stdbuf --output=L ./a.out $inp/GAP-web.mtx        2>&1 | tee -a "$out"
+!stdbuf --output=L ./a.out $inp/GAP-kron.mtx       2>&1 | tee -a "$out"
+!stdbuf --output=L ./a.out $inp/GAP-urand.mtx      2>&1 | tee -a "$out"
+!stdbuf --output=L ./a.out $inp/com-Orkut.mtx      2>&1 | tee -a "$out"
+!stdbuf --output=L ./a.out $inp/com-Friendster.mtx 2>&1 | tee -a "$out"
+!stdbuf --output=L ./a.out $inp/arabic-2005.mtx    2>&1 | tee -a "$out"
+!stdbuf --output=L ./a.out $inp/uk-2005.mtx        2>&1 | tee -a "$out"
+!stdbuf --output=L ./a.out $inp/webbase-2001.mtx   2>&1 | tee -a "$out"
+!stdbuf --output=L ./a.out $inp/it-2004.mtx        2>&1 | tee -a "$out"
+!stdbuf --output=L ./a.out $inp/sk-2005.mtx        2>&1 | tee -a "$out"
+!stdbuf --output=L ./a.out $inp/kmer_V1r.mtx       2>&1 | tee -a "$out"
--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-src="louvain-openmp-static-vs-dynamic"
-out="/home/resources/Documents/subhajit/$src.log"
+src="graph-openmp"
+out="$HOME/Logs/$src.log"
 ulimit -s unlimited
 printf "" > "$out"
 
@@ -11,16 +11,16 @@ cd $src
 
 # Run
 g++ -std=c++17 -O3 -fopenmp main.cxx
-stdbuf --output=L ./a.out ~/Data/GAP-road.mtx       1 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/GAP-twitter.mtx    0 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/GAP-web.mtx        0 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/GAP-kron.mtx       1 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/GAP-urand.mtx      1 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/com-Orkut.mtx      1 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/com-Friendster.mtx 1 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/arabic-2005.mtx    0 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/uk-2005.mtx        0 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/webbase-2001.mtx   0 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/it-2004.mtx        0 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/sk-2005.mtx        0 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/kmer_V1r.mtx       1 2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/GAP-road.mtx       2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/GAP-twitter.mtx    2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/GAP-web.mtx        2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/GAP-kron.mtx       2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/GAP-urand.mtx      2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/com-Orkut.mtx      2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/com-Friendster.mtx 2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/arabic-2005.mtx    2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/uk-2005.mtx        2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/webbase-2001.mtx   2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/it-2004.mtx        2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/sk-2005.mtx        2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/kmer_V1r.mtx       2>&1 | tee -a "$out"
