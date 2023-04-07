320a5a436982da540d7e63c19370b46ef85665c4
Subhajit Sahu <wolfram77@gmail.com>
2023-04-07 12:09:39 +0530

    :bug: debug zero degree issue

--- main.cxx
+++ main.cxx
@@ -15,16 +15,23 @@ int main(int argc, char **argv) {
   using K = uint32_t;
   using V = TYPE;
   char *file = argv[1];
-  bool sym   = argc>2? stoi(argv[2]) : false;
-  int repeat = argc>3? stoi(argv[3]) : 5;
-  OutDiGraph<K, None, V> x, y;  // V w = 1;
-  LOG("Loading graph %s ...\n", file);
   omp_set_num_threads(MAX_THREADS);
   LOG("OMP_NUM_THREADS=%d\n", MAX_THREADS);
-  // float tx = measureDuration([&]() { readMtxW(x, file); });
-  // println(x); printf("[%09.3f ms] readMtxW\n", tx); x.clear();
-  float ty = measureDuration([&]() { readMtxOmpW(y, file, true); });
-  LOG(""); println(y); LOG("[%09.3f ms] readMtxOmpW\n", ty); y.clear();
+  OutDiGraph<K> x;
+  auto fl = [](auto u) { return true; };
+  LOG("Loading graph %s ...\n", file);
+  readMtxW(x, file);              LOG(""); println(x);
+  x = selfLoopOmp(x, None(), fl); LOG(""); println(x);
+  x.forEachVertexKey([&](auto u) {
+    K vd = K();
+    if (x.degree(u)!=2) return;
+    x.forEachEdgeKey(u, [&](auto v) { if (v!=u) vd = v; });
+    x.removeEdge(u, vd);
+  });
+  updateOmpU(x);
+  x.forEachVertexKey([&](auto u) {
+    if (x.degree(u)==0) LOG("x.degree(%d)==0\n", u);
+  });
   printf("\n");
   return 0;
 }
