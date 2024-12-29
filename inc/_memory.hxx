c6e4ea5b5a8498c3ec4873c64ff0583826313615
Subhajit Sahu <wolfram77@gmail.com>
2024-12-29 12:08:03 +0530

    :bug: test arena allocators
    Concurrent arena allocator is not efficient!

--- inc/_memory.hxx
+++ inc/_memory.hxx
@@ -1,10 +1,18 @@
 #pragma once
 #include <cstdint>
+#include <atomic>
 #include <vector>
+#include <thread>
 #include <algorithm>
 #include "_mman.hxx"
 
+using std::memory_order_relaxed;
+using std::memory_order_acquire;
+using std::memory_order_release;
+using std::atomic_flag;
+using std::atomic;
 using std::vector;
+using std::this_thread::yield;
 using std::min;
 
 
@@ -12,11 +20,13 @@ using std::min;
 
 #pragma region CLASSES
 /**
- * A fixed-capacity Arena Allocator.
+ * A thread-safe Arena Allocator, that supports fast allocation and deallocation.
  * @tparam SIZE size of each allocation
+ * @tparam MIN_FREED minimum number of freed allocations to keep [128]
+ * @note Use for allocations larger than cache line size to avoid false sharing.
  */
-template <size_t SIZE>
-class FixedArenaAllocator {
+template <size_t SIZE, size_t MIN_FREED=128>
+class ConcurrentArenaAllocator {
   #pragma region CONSTANTS
   public:
   /** Size of each allocation. */
@@ -25,34 +35,41 @@ class FixedArenaAllocator {
 
 
   #pragma region DATA
-  /** The memory pool. */
-  void  *pool;
-  /** Size of the memory pool. */
-  size_t capacity;
-  /** Number of bytes used in the memory pool. */
-  size_t used;
   /** These allocations have been freed, and can be reused. */
   vector<void*> freed;
+  /** Mutex to protect the freed list. */
+  atomic_flag mutex {};
+  /** The memory pool. */
+  void  *pool;
+  /** Number of bytes used in the memory pool. */
+  atomic<size_t> used = 0;
+  /** Size of the memory pool. */
+  size_t capacity;
   #pragma endregion
 
 
   #pragma region METHODS
   public:
   /**
-   * Allocate memory.
+   * Allocate a memory block.
    * @returns allocated memory, or nullptr if out of memory
    */
-  inline void* alloc() {
-    // Allocate from free list, if available.
-    if (!freed.empty()) {
+  inline void* allocate() noexcept {
+    // Allocate from freed list, if available, and no other thread is using it.
+    if (!freed.empty() && !mutex.test_and_set(memory_order_acquire)) {
       void *ptr = freed.back();
       freed.pop_back();
+      mutex.clear(memory_order_release);
       return ptr;
     }
-    // Allocate from pool.
-    if (used < capacity) {
-      void *ptr = (char*) pool + used;
-      used += SIZE;
+    // Else, try to allocate from pool.
+    if (used < capacity) return (char*) pool + used.fetch_add(SIZE, memory_order_relaxed);
+    // Else, must try from freed list.
+    while (!freed.empty()) {
+      if (mutex.test_and_set(memory_order_acquire)) { yield(); continue; }
+      void *ptr = freed.back();
+      freed.pop_back();
+      mutex.clear(memory_order_release);
       return ptr;
     }
     return nullptr;
@@ -60,11 +77,29 @@ class FixedArenaAllocator {
 
 
   /**
-   * Free memory.
+   * Free a memory block.
    * @param ptr memory to free
    */
   inline void free(void *ptr) {
+    // Add to freed list.
+    while (mutex.test_and_set(memory_order_acquire))
+      yield();
     freed.push_back(ptr);
+    mutex.clear(memory_order_release);
+  }
+
+
+  /**
+   * Reset the allocator, freeing all memory.
+   */
+  inline void reset() noexcept {
+    // Reset freed list.
+    while (mutex.test_and_set(memory_order_acquire))
+      yield();
+    freed.clear();
+    mutex.clear(memory_order_release);
+    // Reset used bytes.
+    used.store(0);
   }
   #pragma endregion
 
@@ -72,13 +107,113 @@ class FixedArenaAllocator {
   #pragma region CONSTRUCTORS
   public:
   /**
-   * Create a fixed-capacity Arena Allocator.
+   * Create a new Concurrent Arena Allocator.
    * @param pool memory pool
    * @param capacity size of memory pool
    */
-  FixedArenaAllocator(void *pool, size_t capacity) :
-  pool(pool), capacity(capacity), used(0) {
-    freed.reserve(min(capacity/SIZE, size_t(128)));
+  ConcurrentArenaAllocator(void *pool, size_t capacity)
+  : pool(pool), capacity(capacity) {
+    // Reserve space for freed list.
+    freed.reserve(min(capacity/SIZE, MIN_FREED));
+  }
+  #pragma endregion
+};
+
+
+
+
+/**
+ * A Recursive Arena Allocator, that supports fast allocation and deallocation.
+ * @tparam SIZE size of each allocation
+ * @tparam MIN_FREED minimum number of freed allocations to keep [32]
+ * @note This allocator is not thread-safe. Use for allocations smaller than cache line size, on each thread.
+ */
+template <size_t SIZE, size_t MIN_FREED=32>
+class RecursiveArenaAllocator {
+  #pragma region CONSTANTS
+  public:
+  /** Size of each allocation. */
+  static constexpr size_t allocation_size = SIZE;
+  #pragma endregion
+
+
+  #pragma region DATA
+  /** These allocations have been freed, and can be reused. */
+  vector<void*> freed;
+  /** The memory pools. */
+  vector<void*> pools;
+  /** Number of bytes used in the last memory pool. */
+  size_t used = 0;
+  /** Size of each memory pool. */
+  size_t capacity;
+  #pragma endregion
+
+
+  #pragma region METHODS
+  public:
+  /**
+   * Allocate a memory block.
+   * @param fallocate function to allocate a memory pool, e.g., fallocate(capacity)
+   * @returns allocated memory, or nullptr if out of memory
+   */
+  template <class FA>
+  inline void* allocate(FA fallocate) {
+    // Allocate from freed list, if available.
+    if (!freed.empty()) {
+      void *ptr = freed.back();
+      freed.pop_back();
+      return ptr;
+    }
+    // Else, try to allocate from last pool.
+    if (!pools.empty() && used < capacity) {
+      void *ptr = (char*) pools.back() + used;
+      used += SIZE;
+      return ptr;
+    }
+    // Else, must allocate a new pool.
+    void *pool = fallocate(capacity);
+    pools.push_back(pool);
+    used = SIZE;
+    return pool;
+  }
+
+
+  /**
+   * Free a memory block.
+   * @param ptr memory to free
+   */
+  inline void free(void *ptr) {
+    freed.push_back(ptr);
+  }
+
+
+  /**
+   * Reset the allocator, freeing all memory.
+   * @param ffree function to free a memory pool, e.g., ffree(pool)
+   */
+  template <class FF>
+  inline void reset(FF ffree) noexcept {
+    // Reset freed list, used bytes.
+    freed.clear();
+    used = 0;
+    // Free all memory pools.
+    for (void *pool : pools)
+      ffree(pool);
+    pools.clear();
+  }
+  #pragma endregion
+
+
+  #pragma region CONSTRUCTORS
+  public:
+  /**
+   * Create a new Recursive Arena Allocator.
+   * @param capacity size of each memory pool
+   */
+  RecursiveArenaAllocator(size_t capacity)
+  : capacity(capacity) {
+    // Reserve space for freed list.
+    freed.reserve(min(capacity/SIZE, MIN_FREED));
   }
   #pragma endregion
 };
--- main.cxx
+++ main.cxx
@@ -36,7 +36,7 @@ using namespace std;
 
 #pragma region METHODS
 #pragma region PERFORM EXPERIMENT
-int main(int argc, char **argv) {
+int mainTestGraph(int argc, char **argv) {
   using K = uint32_t;
   using V = None;
   using E = TYPE;
@@ -69,6 +69,44 @@ int main(int argc, char **argv) {
     printf("{%09.1fms} %s\n", tr, "readMtxFormatFileToGraphOmpW");
   }
   printf("\n");
+  return 0;
+}
+
+
+int main(int argc, char **argv) {
+  omp_set_num_threads(MAX_THREADS);
+  LOG("OMP_NUM_THREADS=%d\n", MAX_THREADS);
+  constexpr size_t EACH  = 64;
+  constexpr size_t COUNT = 1024 * 1024 * 256;
+  // Test libc memory allocation/deallocation performance.
+  vector<void*> ptrs(COUNT);
+  float t0 = measureDuration([&]() {
+    #pragma omp parallel for schedule(dynamic, 2048)
+    for (int i=0; i<COUNT; ++i)
+      ptrs[i] = malloc(EACH);
+  });
+  printf("Allocated %zu blocks of %zu bytes in %.3fms, using libc\n", COUNT, EACH, t0);
+  float t1 = measureDuration([&]() {
+    #pragma omp parallel for schedule(dynamic, 2048)
+    for (int i=0; i<COUNT; ++i)
+      free(ptrs[i]);
+  });
+  printf("Freed %zu blocks of %zu bytes in %.3fms, using libc\n", COUNT, EACH, t1);
+  // Test Arena memory allocation/deallocation performance.
+  ptrs.clear();
+  auto t2 = timeNow();
+  void *pool = mmapAlloc(COUNT * EACH);
+  ConcurrentArenaAllocator<EACH> arena(pool, EACH*COUNT);
+  #pragma omp parallel for schedule(dynamic, 2048)
+  for (int i=0; i<COUNT; ++i)
+    ptrs.push_back(arena.allocate());
+  auto t3 = timeNow();
+  printf("Allocated %zu blocks of %zu bytes in %.3fms, using arena allocator\n", COUNT, EACH, duration(t2, t3));
+  #pragma omp parallel for schedule(dynamic, 2048)
+  for (int i=0; i<COUNT; ++i)
+    arena.free(ptrs[i]);
+  auto t4 = timeNow();
+  printf("Freed %zu blocks of %zu bytes in %.3fms, using arena allocator\n", COUNT, EACH, duration(t3, t4));
   return 0;
 }
 #pragma endregion
