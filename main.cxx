c58ff7ee3005cc2d64d5af3b23628e3cc3d9444f
Subhajit Sahu <wolfram77@gmail.com>
2025-01-04 10:56:53 +0530

    :bug: use CSR to populate the graph

--- main.cxx
+++ main.cxx
@@ -59,6 +59,32 @@ int main(int argc, char **argv) {
     LOG(""); println(xc);
     printf("{%09.1fms} %s\n", tr, "readMtxFormatToCsrOmpW");
   }
+  // Read graph-ps in parallel.
+  {
+    DiGraph<K, V, E> x;
+    MappedFile mf(file);
+    size_t size = mf.size();
+    string_view data((const char*) mf.data(), mf.size());
+    double tr = measureDuration([&]() {
+      if (weighted) readMtxFormatToGraphPsOmpW<true> (x, data);
+      else          readMtxFormatToGraphPsOmpW<false>(x, data);
+    });
+    LOG(""); println(x);
+    printf("{%09.1fms} %s\n", tr, "readMtxFormatToGraphFromCsrOmpW");
+  }
+  // Read graph-ps in parallel.
+  {
+    // DiGraph<K, V, E> x;
+    // MappedFile mf(file);
+    // size_t size = mf.size();
+    // string_view data((const char*) mf.data(), mf.size());
+    // double tr = measureDuration([&]() {
+    //   if (weighted) readMtxFormatToGraphSpOmpW<true> (x, data);
+    //   else          readMtxFormatToGraphSpOmpW<false>(x, data);
+    // });
+    // LOG(""); println(x);
+    // printf("{%09.1fms} %s\n", tr, "readMtxFormatToGraphFromEdgelistsOmpW");
+  }
   // Read graph in parallel.
   {
     DiGraph<K, V, E> x;
